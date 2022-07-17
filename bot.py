4c5cce7089cc8dc179228f968472fd61e9bb3104
PeeusD <2014pgmca076@nitjsr.ac.in>
2022-07-18 00:07:57 +0530

    added fillers

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
+from multiprocessing import Condition
 from Pos import Adjective,Adverb,Articles,Conditional,Narration, Preposition,Noun,Pronoun,Sub_Agrmt,Tense,verb
+from Fillers import Fill_pt1,Fill_pt2,Fill_pt3,Fill_pt4,Fill_pt5,Fill_pt6
 from time import sleep
 from telegram.ext import Updater, CommandHandler, MessageHandler,Filters, CallbackQueryHandler
 from telegram import ChatAction, Bot, ParseMode, Update, InlineKeyboardButton, InlineKeyboardMarkup
@@ -16,125 +18,78 @@ bot = Bot(token=TOKEN)
 updater = Updater(TOKEN)
 
 
-def adjective_query():   #####  <--------------  CHANGE HERE FOR DEBUGGING  ------>
-    for i in range(52):    
-            sleep(5)
-            ques = Adjective.questions[i][0]
-            print(ques)
-            sleep(10)
-            ans = Adjective.sol[i] 
-            print(ans)
+def questions_func(module_name):   #####  <--------------  CHANGE HERE FOR DEBUGGING  ------>
+    if "Narration" in str(module_name): length = 20
+    elif "Fill" in str(module_name): length = 30 
+    else: length = 52
 
-def adverb_query():
-    for i in range(52): 
-        sleep(5)       
-        ques = Adverb.questions[i][0]
-        # print(ques)
-        bot.send_message(chat_id = chat_id, text = ques, parse_mode = ParseMode.HTML)
-        sleep(10)
-        ans = Adverb.sol[i]
-        # print(ans)
-        bot.send_message(chat_id = chat_id, text = ans, parse_mode = ParseMode.HTML)
-def articles_query():
-    for i in range(52):  
-        sleep(5)  
-        ques = Articles.questions[i][0]
-        print(ques)
-        sleep(10)
-        ans = Articles.sol[i]
-        print(ans)
-def condtional_query():
-    for i in range(52):   
-        sleep(5) 
-        ques = Conditional.questions[i][0]
-        print(ques)
-        sleep(10)
-        ans = Conditional.sol[i]
-        print(ans)
-def verb_query():
-    for i in range(52):
+    for i in range(length):    
+        bot.send_chat_action(chat_id = chat_id, action = ChatAction.TYPING)
         sleep(5)
-        ques = verb.questions[i][0]
-        print(ques)
+        ques = '<b>' + module_name.questions[i][0] + '</b>'
+        
+        bot.send_message(chat_id=chat_id, text=ques, parse_mode=ParseMode.HTML)
         sleep(10)
-        ans = verb.sol[i]
-        print(ans)
-def noun_query():
-    for i in range(52):
-        sleep(5)
-        ques = Noun.questions[i][0]
-        print(ques)
-        sleep(10)
-        ans = Noun.sol[i]
-        print(ans)
-def narr_query():
-    for i in range(20):
-        sleep(5)
-        ques = Narration.questions[i][0]
-        print(ques)
-        sleep(10)
-        ans = Narration.sol[i]
-        print(ans)
-def preposition_query():
-    for i in range(52):
-        sleep(5)
-        ques = Preposition.questions[i][0]
-        print(ques)
-        sleep(10)
-        ans = Preposition.sol[i]
-        print(ans)
-def pronoun_query():
-    for i in range(52):
-        sleep(5)
-        ques = Pronoun.questions[i][0]
-        print(ques)
-        sleep(10)
-        ans = Pronoun.sol[i]
-        print(ans)
-def subAgrmt_query():
-    for i in range(52): 
-        sleep(5)       
-        ques = Sub_Agrmt.questions[i][0]
-        print(ques)
-        sleep(10)
-        ans = Sub_Agrmt.sol[i]
-        print(ans)
-def tense_query():
-    for i in range(52):  
-        sleep(5)  
-        ques = Tense.questions[i][0]
-        print(ques)
-        sleep(10)
-        ans = Tense.sol[i]
-        print(ans)
+        ans =  '<i>' + module_name.sol[i] + '</i>' 
+        bot.send_message(chat_id=chat_id, text=ans, parse_mode=ParseMode.HTML)
 
 
-
-def schedulling1(update, context):
+def english_func(update, context):
     
     query = update.callback_query
     message_text =int(query.data)
     query.answer(text="Test has been Started!✔️")
 
     dct = {
-        1: adjective_query,
-        2: adverb_query,
-        3: articles_query,
-        4: condtional_query,
-        5: verb_query,
-        6: noun_query,
-        7: preposition_query,
-        8: pronoun_query,
-        9: subAgrmt_query,
-        10: tense_query,
-        11: narr_query
+        1: Adjective,
+        2: Adverb,
+        3: Articles,
+        4: Conditional,
+        5: verb,
+        6: Noun,
+        7: Preposition,
+        8: Pronoun,
+        9: Sub_Agrmt,
+        10: Tense,
+        11: Narration,
+        12: Fill_pt1,
+        13: Fill_pt2,
+        14: Fill_pt3,
+        15: Fill_pt4,
+        16: Fill_pt5,
+        17: Fill_pt6
     }
+    questions_func(dct[message_text])
     
-    d = dct[message_text]()
-    return d
+
+def start(update, contest):
+    update.message.reply_text(
+        '*Service is running\! 👍\nPlease select the test:\n/Errordetec :\- Error Detection 50question Sets\n/Fillers :\- Filler sets*',
+        parse_mode='MarkdownV2'
+     )
 
 
-def start(update, context):
+def fillers_kb(update, context):
+    keyboard = [
+        [
+            InlineKeyboardButton("Set-1", callback_data=12),
+            InlineKeyboardButton("Set-2", callback_data=13),
+        ],
+        [
+            InlineKeyboardButton("Set-3", callback_data=14),
+            InlineKeyboardButton("Set-4", callback_data=15),
+        ],
+        [
+            InlineKeyboardButton("Set-5", callback_data=16),
+            InlineKeyboardButton("Set-6", callback_data=17),
+        ],
+       
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('*Please choose only one SET at a time:*', reply_markup=reply_markup, parse_mode='MarkdownV2')
+
+def english_kb(update, context):
 
     keyboard = [
         [
@@ -158,12 +113,14 @@ def start(update, context):
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    update.message.reply_text('*Service is running\! 👍\nPlease choose only one SET at a time:*', reply_markup=reply_markup, parse_mode='MarkdownV2')
-
+    update.message.reply_text('*Please choose only one SET at a time:*', reply_markup=reply_markup, parse_mode='MarkdownV2')
 
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CallbackQueryHandler(schedulling1)) # for getting callback data from keyboard
+updater.dispatcher.add_handler(CommandHandler('Errordetec', english_kb))
+updater.dispatcher.add_handler(CommandHandler('Fillers', fillers_kb))
+updater.dispatcher.add_handler(CallbackQueryHandler(english_func)) # for getting callback data from keyboard
+
 # updater.dispatcher.add_handler(MessageHandler(Filters.chat_type.private, schedulling1)) # for getting data from message
 updater.start_polling()
 updater.idle()
